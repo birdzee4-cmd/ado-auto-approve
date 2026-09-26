a8cc704a3e2dd435e664317f2cbbaa9e89367d11
birdzee4-cmd <birdzee4@gmail.com>
2026-09-26 12:30:50 +0700

    Omit unsupported App Support subject value

--- api/shared/operations-service.js
+++ api/shared/operations-service.js
@@ -222,7 +222,6 @@ function profileFields(supportTeam, incident, primaryWorkItem) {
       'Custom.Deadline': serviceFormDeadline(incident.firstSeen),
       'Custom.ServicePriority': serviceFormPriority(incident),
       'Custom.Country': 'Thai',
-      'Custom.GroupsofSubject': 'อื่น ๆ (Other)',
       'Custom.MonitoringSourceTracker': 'Not Applicable (N/A)',
       'Custom.ActualIncidentTime': incident.firstSeen || null
     };
